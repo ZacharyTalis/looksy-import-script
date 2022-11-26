83059f5ffad3bb693a44b89921a3406153f7b7ba
Zachary Talis <zacharytalis@gmail.com>
2022-11-26 14:04:18 -0500

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# looksy-import-script
+
+Violetmonkey script to import from sequence file (`.seq`) to Looksy Merger.
+
+## Instructions
+
+1. TK
+
+<!-- TODO -->
---
+++ looksy-import.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name        Looksy Import
+// @namespace   https://witness.zacharytalis.com/merge.html
+// @match       https://witness.zacharytalis.com/merge.html
+// @grant       none
+// @version     1.0
+// @author      Zachary Talis
+// @description Import from sequence file to Looksy Merger
+// ==/UserScript==
+
+/////  Update this link to wherever your sequence file is  /////
+const sequenceUrl = "http://127.0.0.1:5500/sequence.seq";
+/////  Update this link to wherever your sequence file is  /////
+
+function clearExistingList(list) {
+    for (var i = list.childElementCount - 1; i >= 0; i--) {
+        deleteData(i);
+    }
+}
+
+function insertBoxAtIndex(index) {
+    insertData(index);
+}
+
+function addLinkAtIndex(link, index) {
+    let boxId = "list-" + index;
+    let box = document.getElementById(boxId);
+    box.value = link;
+    writeData(index, box.value);
+}
+
+function main(text) {
+
+    // Get links from text
+    let lines = text.split("\n");
+    let links = [];
+    for (const line of lines) {
+        if (line.length > 0 && line[0] != "#") {
+            links.push(line);
+        }
+    }
+
+    // Clear list boxes
+    let list = document.getElementById("list");
+    clearExistingList(list);
+
+    // Add to 
+    let index = 0;
+    for (const linkIndex in links) {
+        let link = links[linkIndex];
+        if (index != 0) {
+            insertBoxAtIndex(index);
+        }
+        addLinkAtIndex(link, index);
+        index++;
+    }
+
+}
+
+fetch(sequenceUrl)
+    .then(response => response.text())
+    .then(text => main(text))
+    .catch(error => console.log("Sequence not found at: " + sequenceUrl));
